3d83092d27dbf9ca1be662062d206d69054441ce
Tomasz Smakosz <tomasz.smakosz@gmail.com>
2021-01-03 15:54:02 +0100

    change ts to js

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import classes from './Home.css';
+
+class Home extends Component{
+    render() {
+        return (
+            <div className={classes.Home}>Test</div>
+        );
+    }
+}
+
+export default Home;
