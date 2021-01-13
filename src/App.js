3007ff4521a5c443d0b91a39a671a29471cd51d5
Tomasz Smakosz <tomasz.smakosz@gmail.com>
2021-01-13 21:37:32 +0100

    added some elements from previous project created demo view of homepage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import classes from './App.css';
 import React, { Component } from "react";
 import Home from "./containers/Home/Home";
+import Layout from "./hoc/Layout/Layout";
 
 function App() {
   return (
     <div className={classes.App}>
-      <Home/>
+        <Layout>
+            <Home/>
+        </Layout>
     </div>
   );
 }
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
+import Aux from '../../hoc/Auxiliary/Auxiliary';
+
+import imgBig from '../../assets/images/django_big.jpg';
+import imgSmall from '../../assets/images/django_small.jpg';
 
 import classes from './Home.css';
 
 class Home extends Component{
     render() {
         return (
-            <div className={classes.Home}>Test</div>
+            <Aux>
+                <div className={classes.TrailerReview}>
+                    <div className={classes.Trailer}>
+                        <img src={imgBig} className={classes.MovieTrailerImage}/>
+                        <img src={imgSmall} className={classes.MovieImageCard}/>
+                        <div className={classes.MovieTrailerImageGradient}/>
+                    </div>
+                    <div className={classes.ReviewList}>
+                        <span>Reviews</span>
+                        <div className={classes.Review}>
+                            <div className={classes.ReviewGradient}/>
+                        </div>
+                        <div className={classes.Review}>
+                            <div className={classes.ReviewGradient}/>
+                        </div>
+                    </div>
+                </div>
+                <div className={classes.MovieTrailerCarousel}>
+                    <img src={imgSmall} className={classes.MovieImage}/>
+                    <img src={imgSmall} className={classes.MovieImage}/>
+                    <img src={imgSmall} className={classes.MovieImage}/>
+                    <img src={imgSmall} className={classes.MovieImage}/>
+                    <img src={imgSmall} className={classes.MovieImage}/>
+                    <img src={imgSmall} className={classes.MovieImage}/>
+                    <div className={classes.SelectionFrame}/>
+                    <div className={classes.ChevronLeft}/>
+                    <div className={classes.ChevronRight}/>
+                </div>
+            </Aux>
         );
     }
 }
